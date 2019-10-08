b4df08f3b32dc8ed0f58224ae63fecb954c0f101
Đào Phong Trúc Đại <mac@Macs-MacBook-Pro-4.local>
2019-10-09 01:04:01 +0700

    1009

--- api/models/SneakersModel.js
+++ api/models/SneakersModel.js
@@ -2,6 +2,16 @@ const mongoose=require('mongoose');
 const model=mongoose.model;
 const Schema=mongoose.Schema;
 
+const commentSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+        content: { type: String, required: true },
+        
+    },
+    { timestamps: { createdAt: "createdAt" } }
+);
+const commentModel=model('Comment',commentSchema);
+
 const SneakersSchema=new Schema({
     name:{
         type:String
@@ -10,14 +20,20 @@ const SneakersSchema=new Schema({
         type:Schema.Types.ObjectId,
         ref:'Brand'
     },
-    prize:{
+    price:{
         type:Number
     },
     size:[{
         type: String,
         enum:['8','8.5','9','9.5','10','10.5','11','11.5','12']
     }],
-    image:String
+    image:String,
+    comment:{
+        type:[commentSchema],
+        default:[]
+    }
 });
-
-module.exports=model('Sneakers',SneakersSchema);
+const sneakersModel=model('Sneakers',SneakersSchema);
+module.exports={
+    commentModel,sneakersModel
+}
--- api/routes/SneakersRouter.js
+++ api/routes/SneakersRouter.js
@@ -1,4 +1,5 @@
-const SneakersModel=require('../models/SneakersModel');
+const SneakersModel=require('../models/SneakersModel').sneakersModel;
+const CommentModel=require('../models/SneakersModel').commentModel;
 const express=require('express');
 const router= express.Router();
 
@@ -55,6 +56,47 @@ router.delete('/:id',(req,res)=>{
         }
         else{
             res.status(204).json('Sneakers Successfully Deleted');
+        }
+    })
+})
+
+router.post('/:id/comment',(req,res)=>{
+    const id=req.params.id;
+    SneakersModel.findById(id,(err,sneakersFound)=>{
+        if(err)res.status(500).json(err);
+        else if(!sneakersFound){
+            res.status(404).json('Sneakers Not Found');
+        }
+        else {
+            CommentModel.create(req.body,(err,commentCreated)=>{
+                if(err)res.status(500).json(err);
+                else {
+                   sneakersFound['comment'].push(commentCreated);
+                   sneakersFound.save((err) =>{
+                       if(err){res.send('Cannot save')}
+                   });
+                   res.status(201).json(commentCreated);
+                }              
+            })
+        }
+    })
+})
+/*req.body:{
+    user:user_id
+    content:String
+}*/
+router.get('/:id/comment',(req,res)=>{
+    const id=req.params.id;
+    SneakersModel
+    .findById(id)
+    .populate('comment.user')
+    .exec((err,sneakersWithCommentsPopulated)=>{
+        if(err)res.status(500).json(err);
+        else if(!sneakersWithCommentsPopulated){
+            res.status(404).json('Sneakers Not Found')
+        }
+        else{
+        res.status(200).json(sneakersWithCommentsPopulated);
         }
     })
 })
